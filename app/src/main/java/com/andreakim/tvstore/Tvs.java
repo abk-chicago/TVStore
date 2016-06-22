a6da2e766ae119c5c0c76f033393bf3bf8b36867
Andrea Kim <andreaforanimals@gmail.com>
2016-06-22 12:32:35 -0500

    adding class and g/s

---
+++ app/src/main/java/com/andreakim/tvstore/Tvs.java
@@ -0,0 +1,43 @@
+package com.andreakim.tvstore;
+
+/**
+ * Created by andreakim on 6/22/16.
+ */
+public class Tvs {
+    public int id;
+    public String brand;
+    public String size;
+    public int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
